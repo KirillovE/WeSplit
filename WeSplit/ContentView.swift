c9e77f08f674b14278065e918f88269c36391dbf
Evgeniy Kirillov <eskirillov@avito.ru>
2021-09-07 08:15:13 +0300

    Check amount input

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var chekAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    private let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section {
+                TextField("Amount", text: $chekAmount)
+                    .keyboardType(.decimalPad)
+            }
+            
+            Section {
+                Text("$\(chekAmount)")
+            }
+        }
     }
 }
 
