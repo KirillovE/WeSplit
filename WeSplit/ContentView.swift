71ea9965735b9d418ce07c6b6b67f781f121561c
Evgeniy Kirillov <eskirillov@avito.ru>
2021-09-07 08:55:21 +0300

    Showing totaler person

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,17 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var chekAmount = ""
+    @State private var checkAmount = ""
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 2
     
     private let tipPercentages = [10, 15, 20, 25, 0]
     
+    private var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    TextField("Amount", text: $chekAmount)
+                    TextField("Amount", text: $checkAmount)
                         .keyboardType(.decimalPad)
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2 ..< 100) {
@@ -38,7 +50,7 @@ struct ContentView: View {
                 .pickerStyle(SegmentedPickerStyle())
                 
                 Section {
-                    Text("$\(chekAmount)")
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
                 }
             }
             .navigationTitle("WeSplit")
